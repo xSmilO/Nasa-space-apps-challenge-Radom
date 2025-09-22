d608d1ce4eba87cb4f55001f5c841770958285a7
xSmilO <mateosmigiel2005@gmail.com>
2025-09-22 16:52:27 +0200

    three setup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import * as THREE from "three";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.SphereGeometry();
+const material = new THREE.MeshStandardMaterial({
+    wireframe: true,
+    color: 0x00ff00,
+});
+const earth = new THREE.Mesh(geometry, material);
+scene.add(earth);
+
+camera.position.z = 5;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enabled = true;
+
+function animate() {
+    renderer.render(scene, camera);
+}
+
